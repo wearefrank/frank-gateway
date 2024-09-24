887b020b735d1b933572be3f6602b36d5db67171
yelga <y.elganayny@hotmail.com>
2024-09-24 11:28:59 +0200

    several small plugin changes

---
+++ src/apisix/plugins/jwt-client.lua
@@ -0,0 +1,137 @@
+local core = require("apisix.core")
+local url  = require("net.url")
+local json = require("apisix.core.json")
+
+local token_cache = ngx.shared["jwt-client-cache"]
+
+local plugin_name = "jwt-client"
+
+local schema = {
+	type = "object",
+	properties = {
+		token_endpoint = {
+			type = "string"
+		},
+		client_id_field_name = {
+			description = "Name for the field equivalent to client_id",
+			type = "string"
+		},
+		client_id_value = {
+			description = "value for the parameter with the name defined in 'client_id_field_name'",
+			type = "string"
+		},
+        default_expiration = {
+			type = "integer",
+			minimum = 1,
+			maximum = 100000,
+			default = 600,
+			description = "default expiration of cached tokens, when expiration is not provided by IDP in response token"
+		},
+		custom_parameters = {
+			description = "Set your own parameters for token request",
+			type = "object",
+			minProperties = 1,
+			patternProperties = {
+				["^[^:]+$"] = {
+					oneOf = {
+						{ type = "string" },
+						{ type = "number" }
+					}
+				}
+			}
+		}
+    },
+	required = {"client_id_field_name", "client_id_value", "token_endpoint"}
+}
+
+local metadata_schema = {}
+
+local _M = {
+	version = 0.1,
+	priority = 1,
+	name = plugin_name,
+	schema = schema,
+	metadata_schema = metadata_schema
+}
+
+function _M.check_schema(conf, schema_type)
+	if schema_type == core.schema.TYPE_METADATA then
+		return core.schema.check(metadata_schema, conf)
+	end
+	return core.schema.check(schema, conf)
+end
+
+function _M.access(conf, ctx)
+
+	local client_id_name = conf.client_id_field_name
+	local client_id_value = conf.client_id_value
+	local token_endpoint = conf.token_endpoint
+	local custom_params = conf.custom_parameters
+
+	local cached_token = token_cache:get(client_id_value)
+	if cached_token ~= nil then
+		core.log.info("JWT client found token in cache, using cached token")
+		core.request.add_header(ctx, "Authorization", "Bearer " .. cached_token)
+		return
+	end
+
+	local parsed_url = url.parse(token_endpoint)
+
+	local httpc = assert(require('resty.http').new())
+	core.log.info("JWT client before connect", parsed_url)
+	local ok, err = httpc:connect({
+		ssl_verify = false,
+		scheme = parsed_url.scheme,
+		host = parsed_url.host,
+		port = parsed_url.port,
+	})
+	core.log.info("JWT client after connect", err)
+
+	local request_body = "{" .. "\"" .. client_id_name .. "\"" .. ":" .. "\"" .. client_id_value .. "\""
+	if custom_params ~= nil then
+		for param, value in pairs(custom_params) do
+			request_body = request_body .. "," .. "\"" .. param .. "\"" .. ":" .. "\"" .. value .. "\""
+		end
+	end
+    request_body = request_body .. "}"
+
+	core.log.info("JWT client build request: " ..  request_body)
+
+	if ok and not err then
+		local response, call_err = assert(httpc:request {
+			method = 'POST',
+			path = parsed_url.path,
+			body = request_body,
+			headers = {
+				["Content-Type"] = "application/json",
+			},
+		})
+		core.log.info("JWT client request")
+
+		if call_err ~= nil or response.status ~= 200 then
+			err = "getting token failed"
+		end
+        core.log.info("JWT client IDP response status: ", response.status)
+
+        local body, err = response:read_body()
+		if err then
+			core.log.error(err)
+		end
+ 
+		local token_response = core.json.decode(body)
+
+		token_cache:set(client_id_value, token_response.token, conf.default_expiration)
+		core.log.info("JWT client token Cached: " .. token_response.token)
+
+		core.request.add_header(ctx, "Authorization", "Bearer " .. token_response.token)
+		core.log.info("JWT client full request: " .. core.request)
+	end
+
+	if err then
+		core.log.error("JWT client connection error: ", err)
+	end
+
+	httpc:close()
+end
+
+return _M
