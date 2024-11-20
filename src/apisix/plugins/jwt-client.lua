957aeb8dfa1ce61a214842aa9b0378034e03b5ba
yelga <y.elganayny@hotmail.com>
2024-11-20 15:06:41 +0100

    reverted too far

--- src/apisix/plugins/jwt-client.lua
+++ src/apisix/plugins/jwt-client.lua
@@ -83,6 +83,7 @@ function _M.access(conf, ctx)
 		scheme = parsed_url.scheme,
 		host = parsed_url.host,
 		port = parsed_url.port,
+		ssl_server_name = parsed_url.host
 	})
 	core.log.info("JWT client after connect", err)
 
@@ -102,7 +103,7 @@ function _M.access(conf, ctx)
 			path = parsed_url.path,
 			body = request_body,
 			headers = {
-				["Content-Type"] = "application/json",
+				["Content-Type"] = "application/json"
 			},
 		})
 		core.log.info("JWT client request")
