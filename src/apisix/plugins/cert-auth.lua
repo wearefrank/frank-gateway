288dea05b52fba769b724b76ef360378831985a1
Sharoek <sharoek@live.nl>
2025-06-19 16:12:11 +0200

    - Added custom `cert-auth` plugin to support client certificate-based authentication. - Extracts the Common Name (CN) from the client certificate subject DN. - Looks up matching consumer based on CN value. - Attaches matched consumer to request context using `consumer_mod.attach_consumer`. - Explicitly sets `ctx.consumer_name` to ensure compatibility with `consumer-restriction` plugin. - Configured route to enforce authentication and consumer restriction. - added test certificates to be used and documentation. - Upgraded apisix from 3.8 to 3.12
    Verified functionality with successful authentication and rejection of unauthorized clients.

--- src/apisix/plugins/cert-auth.lua
+++ src/apisix/plugins/cert-auth.lua
@@ -1,69 +1,76 @@
 local core = require("apisix.core")
 local url  = require("net.url")
-
-local plugin_name = "openid-connect-client" /**/
+local plugin_name = "cert-auth"
+local consumer_mod = require("apisix.consumer")
 
 local schema = {
-	type = "object",
-	properties = {
-		token_endpoint = {/**/
-			type = "string"
-		},
-		client_id = {
-			type = "string"/**/
-		}
-	},
-	required = {"grant_type"}/**/
 }
 
-local metadata_schema = {}
+local consumer_schema = {
+	type = "object",
+	properties = {
+		cn = {type = "string"},
+	},
+	required = {"cn"},
+}
 
 local _M = {
 	version = 0.1,
-	priority = 1,
+	priority = 2401,
 	name = plugin_name,
+	type = "auth", -- This means authentication plugin
 	schema = schema,
-	metadata_schema = metadata_schema
+	consumer_schema = consumer_schema
 }
 
+-- If its an authentication plugin then it needs to choose a consumer after execution
+-- To interact with the consumer resource, this type of plugin needs to provide a consumer_schema to check the plugins configuration in the consumer.
+-- The consumer schema is used to create the consumer object.
+
 function _M.check_schema(conf, schema_type)
-	if schema_type == core.schema.TYPE_METADATA then
-		return core.schema.check(metadata_schema, conf)
+	if schema_type == core.schema.TYPE_CONSUMER then
+		return core.schema.check(consumer_schema, conf)
 	end
 	return core.schema.check(schema, conf)
 end
 
 function _M.access(conf, ctx)
+    local full_dn = ngx.var.ssl_client_s_dn
 
-	local grant_type = conf.grant_type
-	local client_id = conf.client_id
-
-	core.request/**/
-
-	local request_body = "client_id=" .. ngx.escape_uri(client_id) .. "&client_secret=" .. ngx.escape_uri(client_secret)
-	if scope ~= nil then
-		request_body = request_body .. "&scope=" .. ngx.escape_uri(scope)
+	if not ngx.var.ssl_client_cert then
+		return 401, {message = "Client certificate required"}
 	end
 
-	core.log.info("Built request body: " .. request_body)
+	if not full_dn then
+        core.log.warn("Client CN is nil. No client certificate presented or not verified.")
+		return 403, { message = "No client DN found" }
+    else
+        core.log.info("Client CN: ", full_dn)
+    end
 
-	core.log.info("IDP response status: ", res.status)
-	if call_err ~= nil or res.status ~= 200 then
-		err = "getting access token failed"
+	local cn = string.match(full_dn, "CN=([^,/]+)")
+	if not cn then
+		return 403, { message = "Failed to extract CN" }
 	end
-	local body, err = res:read_body()
-	if err then
-		core.log.error(err)
+	core.log.info("Extracted CN: ", cn)
+
+
+	local consumer, consumer_conf, err = consumer_mod.find_consumer(plugin_name, "cn", cn)
+    core.log.info("consumer: ", core.json.delay_encode(consumer))
+    if not consumer then
+		core.log.warn("No consumer found for CN: ", err or "invalid common name")
+    	return 403, {message = "No matching consumer for CN"}
+    end
+
+    consumer_mod.attach_consumer(ctx, consumer, consumer_conf)
+	
+	if consumer and consumer.username then
+    	ctx.consumer_name = consumer.username
+	elseif consumer and consumer.consumer_name then
+		ctx.consumer_name = consumer.consumer_name
 	end
 
-	local expiration = token_response.expires_in or conf.default_expiration
-
-	token_cache:set(client_id, token_response.access_token, expiration)
-	core.request.add_header(ctx, "Authorization", "Bearer " .. token_response.access_token)
-
-	if err then
-		core.log.error(err)
-	end
+	return
 end
 
 return _M
