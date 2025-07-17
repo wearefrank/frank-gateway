2cc1f1643ef2df1cb2ed97159f9e7d23253d9f73
YElGanayny <y.elganayny@hotmail.com>
2025-07-17 11:42:41 +0200

    expand upon frank-sender plugin, allowing most htp methods

--- src/apisix/plugins/frank-sender.lua
+++ src/apisix/plugins/frank-sender.lua
@@ -35,60 +35,67 @@ function _M.check_schema(conf, schema_type)
 	end
 end
 
-
-function _M.access(conf, ctx)
+function _M.access(conf, ctx) -- think about if this needs to be access phase or another phase.
 	
 	local frank_endpoint = conf.frank_endpoint
-	local parsed_url = url.parse(frank_endpoint)
 	
-	local consumer = core.request.header(ctx, "Consumer")
-    if not consumer then
-        core.log.info("No Consumer header found in request. Continuing without.")
-    end
-	core.log.info("Found consumer header: ", consumer)
-
-
-	local httpc = assert(require('resty.http').new())
-	local ok, err = httpc:connect {
-		ssl_verify = false,
-		scheme = parsed_url.scheme,
-		host = parsed_url.host,
-		port = parsed_url.port,
-	}
-
 	ngx_req.read_body()
 	local request_body = req_get_body_data()
 
-	core.log.info("Initial body: " .. request_body .. " ; sending to : " .. frank_endpoint)
-
-	local headers_for_frank = {
-		["Consumer"] = consumer,
-		["Content-Type"] = "application/json"
+	local frank_headers = ngx_req.get_headers()
+	local frank_url = url.parse(frank_endpoint)
+	local request_method = core.request.get_method()
+	local request_params = {
+		path = frank_url.path,
+		method = request_method,
+	}
+	
+	local consumer = core.request.header(ctx, "Consumer")
+    if consumer then
+		core.log.info("Found consumer header: ", consumer)
+		frank_headers["Consumer"] = consumer
+	else
+		core.log.info("No Consumer header found in request. Continuing without.")
+	end
+	
+	local httpc = assert(require('resty.http').new())
+	local ok, err = httpc:connect {
+		ssl_verify = false,
+		scheme = frank_url.scheme,
+		host = frank_url.host,
+		port = frank_url.port,
 	}
 
-	core.log.info("Headers for Frank BEFORE CONTENTTYPE: ", core.json.encode(headers_for_frank))
+	core.log.info("Headers for Frank BEFORE CONTENTTYPE: ", core.json.encode(frank_headers))
 
 	local content_type = core.request.header(ctx, "Content-Type")
 	if content_type then
-		headers_for_frank["Content-Type"] = content_type
+		frank_headers["Content-Type"] = content_type
 		core.log.info("Using content type from header: ", content_type)
 	else
 		core.log.info("No Content-Type header found in request. Using default of application/json.")
 	end
 
-	core.log.info("Headers for Frank AFTER CONTENTTYPE: ", core.json.encode(headers_for_frank))
+	core.log.info("Headers for Frank AFTER CONTENTTYPE: ", core.json.encode(frank_headers))
 
+	if request_body ~= nil then
+		request_params.body = request_body
+		core.log.info("Request body set for Frank: ", request_body)
+	end
+
+	if frank_url.query ~= nil and frank_url.query ~= "" then
+		request_params.query = frank_url.query
+		core.log.info("Query set for Frank: ", frank_url.query)
+	end
+	
 	if ok and not err then
-		local res, call_err = assert(httpc:request {
-			method = 'POST',
-			path = parsed_url.path,
-			body = request_body,
-			headers = headers_for_frank
-		})
+		local res, call_err = assert(httpc:request(request_params))
 		
-		core.log.info("Frank responde code: ", res.status)
-		if call_err ~= nil or res.status ~= 200 then
-			err = "error:" .. call_err "; http code: ".. res.status
+		core.log.info("Frank response code: ", res.status)
+		if call_err ~= nil and res.status ~= 200 then
+			err = "error:" .. call_err .. "; http code: " .. res.status .. ": " .. res.reason
+		elseif res.status ~= 200 then
+			err = "unexpected response code: " .. res.status .. ": " .. res.reason
 		end
 
 		local transformed_body, err = res:read_body()
